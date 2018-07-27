f99a62030db3802407e531ca43bf5390392ecca2
Christian Saborio <csaborio001@users.noreply.github.com>
2018-07-27 15:16:29 +1000

    Add files via upload

---
+++ dashboard-widgets/version-history.php
@@ -0,0 +1,7 @@
+
+<h1>Sample Widget</h1>
+<ul>
+    <li>This can work to deliver any message to your users when they log on to the dashboard.</li>
+    <li>Format in HTML in the PHP files and you are ready to go</li>
+</ul> 
+
